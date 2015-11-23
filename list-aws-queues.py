aae99bb78021589661e324fe98579b26cf4986b3
Dean <c13469208@mydit.ie>
2015-11-23 19:06:59 +0200

    up

--- list-aws-queues.py
+++ list-aws-queues.py
@@ -9,10 +9,17 @@ from boto.sqs.message import Message
 from boto.sqs.connection import SQSConnection
 from boto.exception import SQSError
 import sys
+import urllib2
+import boto
+reponse = urllib2.urlopen('http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key')
+html = reponse.read()
+print(html)
+html1,html2 = html.split(':')
+
 
 # Get the keys from a specific url and then use them to connect to AWS Service 
-access_key_id = ""
-secret_access_key = ""
+access_key_id = html1
+secret_access_key = html2
 
 # Set up a connection to the AWS service. 
 conn = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
@@ -21,3 +28,5 @@ conn = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id=access_key_id, 
 rs = conn.get_all_queues()
 for q in rs:
 	print q.id
+#part 4 
+
